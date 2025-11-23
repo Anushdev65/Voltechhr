a104ba491c3430d8a5e6ba32f3236510d54af95f
Anushdev65 <anushgunner@gmail.com>
2025-11-23 13:09:39 +0545

    Modification on cofig file

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,22 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: "export", // tell Next to create static files in /out
+  trailingSlash: true, // so /about -> /about/index.html (good for shared hosting)
   reactCompiler: true,
   images: {
-    unoptimized: true,
+    unoptimized: true, // disable Next image optimization (required with output: "export")
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
     dangerouslyAllowSVG: true,
   },
 };
